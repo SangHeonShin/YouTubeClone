5a9b8688b0c01a2bdcba03a28196d4294a8b2afb
SangHeonShin <nomadcoders@LAPTOP-A1U2NPS0>
2024-07-22 13:10:49 +0900

    #6.13

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,18 +1,36 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  return res.render("home", { pageTitle: "Home" });
+/* 
+console.log("start");
+Video.find({}, (error, videos) => {
+  if(error){
+    return res.render("server-error");
+  }
+  return res.render("home", { pageTitle: "Home", videos });
+  });
+console.log("finished");
+*/
+
+export const home = async (req, res) => {
+  try {
+    console.log("start");
+    const videos = await Video.find({});
+    console.log(videos);
+    console.log("finished");
+    return res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    return res.render("server-error", { error });
+  }
 };
 export const watch = (req, res) => {
   const { id } = req.params;
 
-  return res.render("watch", { pageTitle: `Watching: ` });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing: ` });
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
