dd3729fe5b87f412565009b47359f5ad23ef3d70
SangHeonShin <nomadcoders@LAPTOP-A1U2NPS0>
2024-08-07 12:06:15 +0900

    #7.18

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -64,7 +64,7 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id: "Ov23liTupg0gpkYXmiMs",
+    client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
@@ -73,8 +73,24 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {
-  res.end();
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const data = await fetch(finalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  console.log(data);
+  const json = await data.json();
+  console.log(json);
 };
 
 export const edit = (req, res) => res.send("Edit User");
