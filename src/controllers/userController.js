323b85371b001dcb3b24e095923c78e94e49d205
SangHeonShin <nomadcoders@LAPTOP-A1U2NPS0>
2024-08-11 12:40:33 +0900

    #8.0

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -214,5 +214,12 @@ export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
 };
+
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
+};
+
 export const see = (req, res) => res.send("See Users");
-export const edit = (req, res) => res.send("Edit User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {
-  edit,
-  remove,
+  getEdit,
+  postEdit,
   logout,
   see,
   startGithubLogin,
@@ -13,12 +13,11 @@ import {
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/kakao/start", startKakaoLogin);
 userRouter.get("/kakao/finish", finishKakaoLogin);
-
 userRouter.get("/:id", see);
 
 export default userRouter;
