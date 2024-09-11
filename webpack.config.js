2b56270297f7312a74c69f1a497f9f912911b047
SangHeonShin <nomadcoders@LAPTOP-A1U2NPS0>
2024-09-11 21:41:21 +0900

    #9.5 SCSS Loader & Separating CSS from JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
-// console.log(__dirname);
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
-// console.log(path.resolve(__dirname, "assets", "js"));
+
 module.exports = {
   entry: "./src/client/js/main.js",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   mode: "development",
   output: {
-    filename: "main.js",
-    path: "./assets/js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
   },
   module: {
     rules: [
@@ -19,6 +23,10 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
