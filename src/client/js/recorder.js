c7dc33157ac8c57ad1b8f4e62cc701008f73a82e
SangHeonShin <nomadcoders@LAPTOP-A1U2NPS0>
2024-09-24 20:24:47 +0900

    #14.0 Introduction

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -24,11 +24,11 @@ const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
+  //
   recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
-    // console.log(event.data);
     videoFile = URL.createObjectURL(event.data);
-    // console.log(videoFile);
+    console.log(videoFile);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
