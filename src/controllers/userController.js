849f5a899c474fb8a278701ad68cbc668a6a7bd7
SangHeonShin <nomadcoders@LAPTOP-A1U2NPS0>
2024-09-25 21:03:12 +0900

    #15.0 Installation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -212,6 +212,7 @@ export const finishKakaoLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   req.session.destroy();
+  req.flash("info", "Bye Bye");
   return res.redirect("/");
 };
 
@@ -272,6 +273,7 @@ export const postEdit = async (req, res) => {
 
 export const getChangePassword = (req, res) => {
   if (req.session.user.socialOnly === true) {
+    req.flash("error", "Can't change password.");
     return res.redirect("/");
   }
   return res.render("users/change-password", { pageTitle: "Change Password" });
@@ -301,6 +303,7 @@ export const postChangePassword = async (req, res) => {
 
   user.password = newPassword;
   await user.save();
+  req.flash("info", "Password Updated");
   return res.redirect("/users/logout");
 };
 
